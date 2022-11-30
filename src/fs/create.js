d458adf2412f26d0d5d0859a9e3e478e79e9bef1
vadim-mg <vadikm1981@yandex.ru>
2022-11-30 12:47:55 +0500

    feat: implement fs:create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,16 @@
-const create = async () => {
-    // Write your code here 
-};
+import { dirname, normalize } from "node:path"
+import { existsSync } from "node:fs"
+import { writeFile } from "node:fs/promises"
 
-await create();
+const fileName = dirname(process.argv[1]) + normalize("/files/fresh.txt")
+
+// create.js - implement function that creates new file fresh.txt with content I am fresh and young inside of the files folder
+const create = async () => {
+  if (existsSync(fileName)) {
+    // if file already exists Error with message "FS operation failed" must be thrown
+    throw Error("FS operation failed")
+  }
+  writeFile(fileName, "I am fresh and young")
+}
+
+await create()
