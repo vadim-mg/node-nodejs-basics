19bf21e9a2eb89948c75adab1fbd168473ee8336
vadim-mg <vadikm1981@yandex.ru>
2022-11-30 20:03:55 +0500

    feat: implement fs:copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,33 @@
-const copy = async () => {
-    // Write your code here 
-};
+import { dirname } from "node:path"
+import { fileURLToPath } from "node:url"
+import { cp } from "node:fs/promises"
 
-copy();
+const fileName = fileURLToPath(import.meta.url)
+const dir = dirname(fileName)
+const sourceDirName = dir + "/files"
+const destDirName = dir + "/files_copy"
+
+/* 
+copy.js - implement function that copies folder "files" with all its content 
+into folder "files_copy" at the same level 
+if files folder doesn't exists or "files_copy" has already been created 
+Error with message 'FS operation failed' must be thrown 
+*/
+
+const copy = async () => {
+  cp(sourceDirName, destDirName, {
+    errorOnExist: true,
+    force: false,
+    recursive: true,
+  })
+    .then((val) => {
+      console.log("Files copied successful!")
+    })
+    .catch((err) => {
+      err.reason = err.message
+      err.message = "FS operation failed"
+      throw err
+    })
+}
+
+copy()
