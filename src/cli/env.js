f7ed340dc082005713c856e34a98dcd3c1fe4ba4
vadim-mg <vadikm1981@yandex.ru>
2022-12-01 14:32:18 +0500

    feat: emplement cli:env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,16 @@
-const parseEnv = () => {
-    // Write your code here 
-};
+/* 
+implement function that parses environment variables with prefix RSS_ 
+and prints them to the console in the format RSS_name1=value1; RSS_name2=value2
+*/
 
-parseEnv();
+const parseEnv = () => {
+  const params = []
+  for (const param in process.env) {
+    if (param.startsWith("RSS_")) {
+      params.push(`${param}=${process.env[param]}`)
+    }
+  }
+  console.log(params.join('; '))
+}
+
+parseEnv()
