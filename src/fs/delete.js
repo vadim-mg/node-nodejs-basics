57e6ee3ec854459a44724b5b26dd24537e131bc6
vadim-mg <vadikm1981@yandex.ru>
2022-12-01 11:39:10 +0500

    feat: implement fs:delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,25 @@
-const remove = async () => {
-    // Write your code here 
-};
+import { rm } from "node:fs/promises"
 
-await remove();
+import { getFullPathName } from "../utils/getFullPathName.js"
+import { fsOperationField } from "../utils/constants.js"
+
+const fileName = getFullPathName(import.meta.url, "/fileToRemove.txt")
+
+/* 
+- implement function that deletes file "fileToRemove.txt" 
+(if there's no file "fileToRemove.txt" Error with message "FS operation failed" must be thrown) 
+*/
+
+const remove = async () => {
+  rm(fileName)
+    .then((val) => {
+      console.log("File was deleted successfully")
+    })
+    .catch((err) => {
+      err.reason = err.message
+      err.message = fsOperationField
+      throw err
+    })
+}
+
+await remove()
