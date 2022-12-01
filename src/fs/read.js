a82721f4ffd34981eedfff9bf1de2b6e0d156357
vadim-mg <vadikm1981@yandex.ru>
2022-12-01 12:13:47 +0500

    feat: emplemet fs:read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,26 @@
-const read = async () => {
-    // Write your code here 
-};
+// import { existsSync } from "node:fs"
+import { readFile } from "node:fs/promises"
 
-await read();
+import { getFullPathName } from "../utils/getFullPathName.js"
+import { fsOperationField } from "../utils/constants.js"
+
+const fileName = getFullPathName(import.meta.url, "/fileToRead.txt")
+
+/* 
+implement function that prints content of the "fileToRead.txt" into console 
+(if there's no file "fileToRead.txt" Error with message "FS operation failed" must be thrown)
+*/
+
+const read = async () => {
+  readFile(fileName, { encoding: "utf8" })
+    .then((result) => {
+      console.log(result)
+    })
+    .catch((err) => {
+      err.reason = err.message
+      err.message = fsOperationField
+      throw err
+    })
+}
+
+await read()
