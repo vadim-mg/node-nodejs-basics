4832a9344c2aa1ca4a42c03f4271addf99c9e50a
vadim-mg <vadikm1981@yandex.ru>
2022-12-01 11:29:27 +0500

    refactor: fs:create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,14 +1,17 @@
 import { existsSync } from "node:fs"
 import { writeFile } from "node:fs/promises"
-import { fileURLToPath } from "node:url"
 
-const fileName = fileURLToPath(import.meta.url)
+import { getFullPathName } from "../utils/getFullPathName.js"
+import { fsOperationField } from "../utils/constants.js"
+
+const fileName = getFullPathName(import.meta.url, "/fresh.txt")
 
 // create.js - implement function that creates new file fresh.txt with content I am fresh and young inside of the files folder
 const create = async () => {
+    console.log(fileName)
   if (existsSync(fileName)) {
     // if file already exists Error with message "FS operation failed" must be thrown
-    throw new Error("FS operation failed")
+    throw new Error(fsOperationField)
   }
   writeFile(fileName, "I am fresh and young")
 }
