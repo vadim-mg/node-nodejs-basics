0c8480e5c2ca3d257740f12371076f43b3838aec
vadim-mg <vadikm1981@yandex.ru>
2022-12-01 11:51:00 +0500

    feat: emplement fs:list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,25 @@
-const list = async () => {
-    // Write your code here 
-};
+import { readdir } from "node:fs/promises"
+import { getFullPathName } from "../utils/getFullPathName.js"
+import { fsOperationField } from "../utils/constants.js"
 
-await list();
+const dir = getFullPathName(import.meta.url)
+
+/* 
+implement function that prints all array of filenames from "files" folder into console 
+(if "files" folder doesn't exists Error with message "FS operation failed" must be thrown) 
+*/
+const list = async () => {
+  readdir(dir)
+    .then((files) => {
+      for (const file of files) {
+        console.log(file)
+      }
+    })
+    .catch((err) => {
+      err.reason = err.message
+      err.message = fsOperationField
+      throw err
+    })
+}
+
+await list()
