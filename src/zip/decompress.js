5d173cbd838002b91b40c5a8c887dc88ea10a90f
vadim-mg <vadikm1981@yandex.ru>
2022-12-03 18:51:53 +0500

    feat: emplement zip

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,19 @@
-const decompress = async () => {
-    // Write your code here 
-};
+/* 
+ implement function that decompresses archive.gz back to the fileToCompress.txt 
+ with same content as before compression using zlib and Streams API
+*/
 
-await decompress();
+import { createUnzip } from "node:zlib"
+import { createReadStream, createWriteStream } from "node:fs"
+import { getFullPathName } from "../utils/getFullPathName.js"
+const srcName = getFullPathName(import.meta.url, "/archive.gz")
+const destName = getFullPathName(import.meta.url, "/fileToCompress.txt")
+
+const decompress = async () => {
+  const unzip = createUnzip()
+  const srcStream = createReadStream(srcName)
+  const destStream = createWriteStream(destName)
+  await srcStream.pipe(unzip).pipe(destStream)
+}
+
+await decompress()
