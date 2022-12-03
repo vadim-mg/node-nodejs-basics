90d9b743a918ffd1b4d30df15452120a9a087755
vadim-mg <vadikm1981@yandex.ru>
2022-12-03 19:30:02 +0500

    fix: stream:transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -2,14 +2,19 @@
  implement function that reads data from process.stdin, 
  reverses text using Transform Stream and then writes it into process.stdout
 */
+import { Transform, pipeline } from "node:stream"
+class ReverseTransform extends Transform {
+  _transform(chunk, encoding, callback) {
+    chunk = [...chunk.toString()]
+    chunk.pop()
+    chunk = chunk.reverse().join("") + "\n"
+    callback(null, chunk)
+  }
+}
 
 const transform = async () => {
-  process.stdin.on("data", (data) => {
-    data = [...data.toString()]
-    data.pop()
-    data = data.reverse().join("") + "\n"
-    process.stdout.write(data)
-  })
+  const reversData = new ReverseTransform()
+  pipeline(process.stdin, reversData, process.stdout, (err) => err)
 }
 
 await transform()
