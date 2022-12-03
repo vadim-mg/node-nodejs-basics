e3a03c86124b74a993917b4e728526f57cbe2a08
vadim-mg <vadikm1981@yandex.ru>
2022-12-03 13:40:43 +0500

    feat: emplement steams

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,15 @@
-const transform = async () => {
-    // Write your code here 
-};
+/* 
+ implement function that reads data from process.stdin, 
+ reverses text using Transform Stream and then writes it into process.stdout
+*/
 
-await transform();
+const transform = async () => {
+  process.stdin.on("data", (data) => {
+    data = [...data.toString()]
+    data.pop()
+    data = data.reverse().join("") + "\n"
+    process.stdout.write(data)
+  })
+}
+
+await transform()
