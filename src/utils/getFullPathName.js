125c0237a87f07f69c24a63ba854aa712d09cd6f
vadim-mg <vadikm1981@yandex.ru>
2022-12-04 16:09:15 +0500

    feat: emplement wt

--- src/utils/getFullPathName.js
+++ src/utils/getFullPathName.js
@@ -1,6 +1,6 @@
 import { dirname, normalize } from "node:path"
 import { fileURLToPath } from "node:url"
 
-export function getFullPathName(url, name = "") {
-  return normalize(dirname(fileURLToPath(url)) + "/files" + name)
+export function getFullPathName(url, name = "", subDir = "/files") {
+  return normalize(dirname(fileURLToPath(url)) + subDir + name)
 }
