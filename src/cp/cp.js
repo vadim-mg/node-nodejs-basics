19eb4a1d3a241fcb15238320bb01bbaa9ac857a4
vadim-mg <vadikm1981@yandex.ru>
2022-12-04 21:52:54 +0500

    feat: emplement childProcess

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,18 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
-};
+/* 
+implement function spawnChildProcess that receives array of arguments args 
+and creates child process from file script.js, 
+passing these args to it. This function should create IPC-channel 
+between stdin and stdout of master process and child process:
+child process stdin should receive input from master process stdin
+child process stdout should send data to master process stdout
+*/
+import { getFullPathName } from "../utils/getFullPathName.js"
+import { fork } from "node:child_process"
 
-spawnChildProcess();
+const fileName = getFullPathName(import.meta.url, "/script.js")
+
+const spawnChildProcess = async (args) => {
+  const child = fork(fileName, args, { stdio: ['inherit', 'inherit', 'ipc'] })
+}
+
+spawnChildProcess(["testArg1", "testArg2"])
